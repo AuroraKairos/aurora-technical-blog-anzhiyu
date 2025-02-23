bd7b605fd43733c9b09ffadbc6fe56bd49200ede
yangchao01 <yangchao01@bestpay.com.cn>
2025-02-23 19:04:47 +0800

    Site updated: 2025-02-23 19:04:46

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/22/hello-world/"];function toRandomPost(){
+var posts=["2025/02/23/Hexo·部署anzhiyu主题/","2025/02/22/Hexo·博客编写手册/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
