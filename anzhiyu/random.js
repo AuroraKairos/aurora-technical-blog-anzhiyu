1ea428172953e46a38ac577331dd1e0174bfb983
yangchao01 <yangchao01@bestpay.com.cn>
2025-02-26 22:44:33 +0800

    Site updated: 2025-02-26 22:44:33

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/22/Hexo·博客编写手册/","2025/02/23/Hexo·部署安知鱼主题/","2025/02/24/技术提升·LeetCode题库/","2025/02/23/环境安装·Node.js/","2025/02/23/环境安装·Git/","2025/02/24/生活·健康指南/"];function toRandomPost(){
+var posts=["2025/02/22/Hexo·博客编写手册/","2025/02/26/Hexo·安知鱼主题-魔改-配置数学公式/","2025/02/23/Hexo·部署安知鱼主题/","2025/02/24/技术提升·LeetCode题库/","2025/02/23/环境安装·Git/","2025/02/24/生活·健康指南/","2025/02/23/环境安装·Node.js/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
